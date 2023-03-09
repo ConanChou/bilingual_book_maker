82d7dccbdbcbd869ded0f8b648756bdfe9823297
Conan <conanchou1412@gmail.com>
2023-03-09 08:03:24 -0500

    optimize prompt

--- book_maker/translator/chatgptapi_translator.py
+++ book_maker/translator/chatgptapi_translator.py
@@ -16,47 +16,35 @@ class ChatGPTAPI(Base):
         openai.api_key = next(self.keys)
 
     def translate(self, text):
-        print(text)
+        print("\n" + text.strip())
         self.rotate_key()
         try:
-            completion = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[
-                    {
-                        "role": "user",
-                        # english prompt here to save tokens
-                        "content": f"Please help me to translate,`{text}` to {self.language}, please return only translated content not include the origin text",
-                    }
-                ],
-            )
-            t_text = (
-                completion["choices"][0]
-                .get("message")
-                .get("content")
-                .encode("utf8")
-                .decode()
-            )
+            t_text = self._call_completion(text)
         except Exception as e:
             # TIME LIMIT for open api please pay
             sleep_time = int(60 / self.key_len)
             time.sleep(sleep_time)
-            print(e, f"will sleep  {sleep_time} seconds")
+            print(e, f"will sleep {sleep_time} seconds")
             self.rotate_key()
-            completion = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[
-                    {
-                        "role": "user",
-                        "content": f"Please help me to translate,`{text}` to {self.language}, please return only translated content not include the origin text",
-                    }
-                ],
-            )
-            t_text = (
-                completion["choices"][0]
-                .get("message")
-                .get("content")
-                .encode("utf8")
-                .decode()
-            )
-        print(t_text)
+            t_text = self._call_completion(text)
+        print(t_text.strip())
         return t_text
+
+    def _call_completion(self, text):
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            temperature=0.4,
+            messages=[
+                {
+                    "role": "user",
+                    "content": f"Translate the given text to {self.language} using the '信、達、雅' as guideline. If the text has no meaning, return the original text as is. Do not translate names. Do not explain. The text to be translated is: {text}.",
+                }
+            ],
+        )
+        return (
+            completion["choices"][0]
+            .get("message")
+            .get("content")
+            .encode("utf8")
+            .decode()
+        )
